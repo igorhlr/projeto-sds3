ab5a80b16b1409cdfd644b6494b568cc9d85d1cb
Igor Henrique <igorhlr@hotmail.com>
2021-11-04 12:35:02 -0300

    Add Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import NavBar from 'components/NavBar';
 import Footer from 'components/Footer';
 import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 
 function App() {
@@ -8,10 +10,27 @@ function App() {
     <>
       <div className="container">
         <NavBar />
-        <h1 className="text-primary">Ola mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Total de vendas</h2>
+
+        </div>
         <DataTable />
       </div>
       <Footer />
+
+
     </>
   );
 }
---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,27 @@
+import Chart from 'react-apexcharts';
+
+const DonutChart = () => {
+
+  const mockData = {
+    series: [477138, 499928, 444867, 220426, 473088],
+    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  }
+
+  const options = {
+    legend: {
+      show: true
+    }
+  }
+
+  return (
+    <Chart
+      //mock = dados sem ser do banco de dados
+      options={{ ...options, labels: mockData.labels }} //
+      series={mockData.series}
+      type="donut"
+      height="240"
+    />
+  );
+}
+
+export default DonutChart;
