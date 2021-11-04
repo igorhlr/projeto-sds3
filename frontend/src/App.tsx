559ed4717b0d4dbca973eee4941449463b4d93d2
Igor Henrique <igorhlr@hotmail.com>
2021-11-04 11:52:33 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
+
 
 function App() {
   return (
-    <div>
-
-      <h1 className="text-primary">Ola mundo!</h1>
-
-    </div>
+    <>
+      <div className="container">
+        <NavBar />
+        <h1 className="text-primary">Ola mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
